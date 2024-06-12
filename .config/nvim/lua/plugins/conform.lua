b76056eaedb03c4082927579c062d3346aa66e26
Louis Van Steene <Louis.VanSteene@softwire.com>
2024-06-12 10:29:55 +0100

    nvim: adjust conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,13 @@
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
-    javascript = { { "prettierd", "prettier" } },
+    javascript = { { "prettierd", "prettier", "eslint" } },
     ruby = { "rubocop" },
     eruby = { "erb-format" }
   },
 
   format_on_save = {
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_fallback = false,
   },
 })
