58564aaab22ae05d6c0c7c728cc97202170ad529
Louis Van Steene <louis@lvs.me.uk>
2024-07-18 23:24:43 +0100

    nvim: ruby indent fix

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,3 +3,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*/node_mod
 -- Enable spell checking for certain file types
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.tex" },
   command = "setlocal spell" })
+
+
+-- stop indent fucking up on .
+vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
