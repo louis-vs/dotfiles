42a53886084b26f6f36949254eff87312c1dcd87
Louis Van Steene <louis@lvs.me.uk>
2024-10-30 17:05:24 +0000

    nvim: adjustments

--- neovim/dot-config/nvim/lua/plugins/telescope.lua
+++ neovim/dot-config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
+      sorting_strategy = "ascending",
       layout_config = {
         horizontal = {
           mirror = false,
