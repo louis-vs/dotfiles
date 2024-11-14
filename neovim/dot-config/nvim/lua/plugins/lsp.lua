da1d1b4965f0c3b882186138ce5ab5502987d28c
Louis Van Steene <louis@lvs.me.uk>
2024-11-14 17:28:01 +0000

    nvim: disable inlay hints by default
    there's a keymap to enable them for a buffer

--- neovim/dot-config/nvim/lua/plugins/lsp.lua
+++ neovim/dot-config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     document_highlight = { enabled = false },
+    inlay_hints = { enabled = false },
     servers = {
       pyright = {
         settings = {
