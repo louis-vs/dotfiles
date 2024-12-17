0bc2a793d8723f8eb6218f786465992aea642194
Louis Van Steene <louis@lvs.me.uk>
2024-12-17 11:14:57 +0000

    nvim: silence vimtex config errors

--- neovim/dot-config/nvim/lua/plugins/latex.lua
+++ neovim/dot-config/nvim/lua/plugins/latex.lua
@@ -1,7 +1,7 @@
 return {
   "lervag/vimtex",
   config = function()
-    vim.go.vimtex_view_method = "skim"
-    vim.go.vimtext_compiler_progname = "nvr"
+    --vim.go.vimtex_view_method = "skim"
+    --vim.go.vimtext_compiler_progname = "nvr"
   end,
 }
