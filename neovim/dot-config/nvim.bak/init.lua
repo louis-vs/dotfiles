530c7df4f71df54964eecfa82a0b0a41da2f2a4e
Louis Van Steene <louis@lvs.me.uk>
2025-04-17 18:05:21 +0100

    nvim: delete old config

--- neovim/dot-config/nvim.bak/init.lua
+++
@@ -1,7 +0,0 @@
-require('config.options')
-require('config.lazy')
-require('config.keymappings')
-require('config.autocmds')
-
-require('lsp.config')
-require('lsp.setup')
--- neovim/dot-config/nvim.bak/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Disable diagnostics in node_modules (0 is current buffer only)
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
--- Enable spell checking for certain file types
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.tex" },
-  command = "setlocal spell" })
-
-
--- stop indent fucking up on .
-vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
