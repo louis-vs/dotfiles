5823d229d57ce5cdcf86f95bb32d51d959fca5c9
Louis Van Steene <louis@lvs.me.uk>
2025-08-06 11:58:24 +0100

    nvim: fix blink

--- neovim/dot-config/nvim/lua/plugins/blink.lua
+++ neovim/dot-config/nvim/lua/plugins/blink.lua
@@ -10,6 +10,18 @@ return {
 
     keymap = {
       preset = "super-tab",
+      -- manually override tab to prevent buggy lazyvim code running
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
       --["<C-y>"] = { "select_and_accept" },
     },
     --menu = {
