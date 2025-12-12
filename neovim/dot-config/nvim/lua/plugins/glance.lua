f11af7fd246cf2b13648589b459c84a83e4f12cd
Louis Van Steene <louis@lvs.me.uk>
2025-12-12 15:59:22 +0000

    nvim: lazyvim 15.x updates

--- neovim/dot-config/nvim/lua/plugins/glance.lua
+++ neovim/dot-config/nvim/lua/plugins/glance.lua
@@ -7,14 +7,17 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
-      vim.list_extend(Keys, {
-        { "gd", "<cmd>Glance definitions<CR>", desc = "Goto Definition", has = "definition" },
-        { "gr", "<cmd>Glance references<CR>", desc = "References" },
-        { "gm", "<cmd>Glance implementations<CR>", desc = "Goto I[m]plementation" },
-        { "gy", "<cmd>Glance type_definitions<CR>", desc = "Goto T[y]pe Definition" },
-      })
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "gd", "<cmd>Glance definitions<CR>", desc = "Goto Definition", has = "definition" },
+            { "gr", "<cmd>Glance references<CR>", desc = "References" },
+            { "gm", "<cmd>Glance implementations<CR>", desc = "Goto I[m]plementation" },
+            { "gy", "<cmd>Glance type_definitions<CR>", desc = "Goto T[y]pe Definition" },
+          },
+        },
+      },
+    },
   },
 }
